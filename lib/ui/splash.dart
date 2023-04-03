711b4dae445eb5e60a8676bc7933ded6ea2aa4eb
kherld-hussein <eternety11@gmail.com>
2023-04-03 20:24:53 +0300

    Update UI

---
+++ lib/ui/splash.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/images/logo.png',
+              scale: 4,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
