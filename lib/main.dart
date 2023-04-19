9ba393759a316bec143b53edbe6d27a6a9fc21f1
kherld-hussein <eternety11@gmail.com>
2023-04-19 16:11:20 +0300

    Implement theme controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get.dart';
 import 'package:supro_vigilant/theme/theme.dart';
+import 'package:supro_vigilant/theme/theme_controller.dart';
 import 'package:supro_vigilant/ui/login_page.dart';
+
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Get.lazyPut<ThemeController>(() => ThemeController());
+  ThemeController.to.getThemeModeFromPreferences();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const SuproApp());
 }
@@ -18,6 +23,8 @@ class SuproApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Supro Vigilante',
       theme: theme,
+      darkTheme: themed,
+      themeMode: ThemeController.to.themeMode,
       debugShowCheckedModeBanner: false,
       home: const LoginPge(),
     );
