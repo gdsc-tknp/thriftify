100411917e37490ac5da4b34f9efdcf5a0e6b284
kherld-hussein <eternety11@gmail.com>
2023-04-19 16:12:59 +0300

    Update UI

--- lib/ui/profile.dart
+++ lib/ui/profile.dart
@@ -2,44 +2,48 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import '../widgets/settings_tile.dart';
+import '../utils/loading_util.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+  const ProfilePage({Key? key, required User user})
+      : _user = user,
+        super(key: key);
+  final User _user;
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  final user = FirebaseAuth.instance.currentUser;
+  late User _user;
 
   @override
   Widget build(BuildContext context) {
-    final user = this.user;
-    // if (user != null) {
-    //   final name = user.displayName;
-    //   final email = user.email;
-    //   final photoUrl = user.photoURL;
-    //
-    //   final emailVerified = user.emailVerified;
-    //
-    //   final uid = user.uid;
-    // }
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Profile'),
-      ),
+      appBar: AppBar(title: const Text('My Profile')),
       body: ListView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 8.0,
-          vertical: 8.0,
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
         children: [
-          const SizedBox(height: 50),
           Column(
             children: [
-              Image.asset('assets/images/logo.png', scale: 30),
+              if (_user.photoURL != null)
+                ClipOval(
+                  child: Material(
+                    child: Image.network(
+                      _user.photoURL!,
+                      fit: BoxFit.fitHeight,
+                      loadingBuilder: (context, child, load) {
+                        if (load == null) return child;
+                        return const LoadingUtil();
+                      },
+                    ),
+                  ),
+                )
+              else
+                Flexible(
+                  flex: 1,
+                  child: Image.asset('assets/images/logo.png', height: 160),
+                ),
               Text(
                 'Supro Vigilante',
                 style: Theme.of(context).textTheme.labelLarge!.copyWith(
@@ -59,29 +63,40 @@ class _ProfilePageState extends State<ProfilePage> {
           const SizedBox(height: 30),
           Card(
             child: Column(
-              children: const [
+              children: [
                 ListTile(
-                  leading: FaIcon(FontAwesomeIcons.user),
-                  title: Text('User'),
-                  trailing: Text('user.uid'),
+                  leading: const FaIcon(FontAwesomeIcons.user),
+                  title: const Text('User'),
+                  trailing: Text(_user.displayName!),
                 ),
-                Divider(
+                const Divider(
                   endIndent: 25,
                   indent: 25,
                 ),
                 ListTile(
-                  leading: FaIcon(FontAwesomeIcons.calendar),
-                  title: Text('Date'),
-                  trailing: Text('User_join_date'),
+                  leading: const FaIcon(FontAwesomeIcons.user),
+                  title: const Text('Email'),
+                  trailing: Text(_user.email!),
                 ),
-                Divider(
+                const Divider(
                   endIndent: 25,
                   indent: 25,
                 ),
                 ListTile(
-                  leading: FaIcon(FontAwesomeIcons.calendarXmark),
-                  title: Text('Time'),
-                  trailing: Text('User_join_time'),
+                  leading: const FaIcon(FontAwesomeIcons.calendar),
+                  title: const Text('Date'),
+                  trailing:
+                      Text(_user.metadata.creationTime!.toUtc().toString()),
+                ),
+                const Divider(
+                  endIndent: 25,
+                  indent: 25,
+                ),
+                ListTile(
+                  leading: const FaIcon(FontAwesomeIcons.calendarXmark),
+                  title: const Text('Time'),
+                  trailing:
+                      Text(_user.metadata.creationTime!.toString().trim()),
                 ),
               ],
             ),
@@ -90,6 +105,10 @@ class _ProfilePageState extends State<ProfilePage> {
       ),
     );
   }
+
+  @override
+  void initState() {
+    _user = widget._user;
+    super.initState();
+  }
 }
-
-
