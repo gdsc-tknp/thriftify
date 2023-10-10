233642aa24c3acbfa925e6a0071129d4eca1629c
kherld-hussein <eternety11@gmail.com>
2023-10-10 03:35:48 +0300

    Create custom splash

--- android/app/src/main/kotlin/com/gdsctknp/supro_vigilant/MainActivity.kt
+++ android/app/src/main/kotlin/com/gdsctknp/supro_vigilant/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.gdsctknp.thriftify
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
 
 class MainActivity: FlutterActivity() {
+    override fun provideSplashScreen(): SplashScreen? = SplashView()
 }
